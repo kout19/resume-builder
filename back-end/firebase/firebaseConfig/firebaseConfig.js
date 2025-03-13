78ed4c5732435c66a79f615f20f6599eb3aa1eff
Kefyalew <koutlook19@gmail.com>
2025-03-12 21:18:06 -0900

    Setting up fireabse

---
+++ back-end/firebase/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,17 @@
+const admin = require("firebase-admin");
+const fs=require('fs');
+const path=require('path');
+require("dotenv").config(); 
+const serviceAccountPath=path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
+console.log("service account path:",serviceAccountPath);
+if(!fs.existsSync(serviceAccountPath)){
+  console.log("error firebase serviec file not found");
+  process.exit(1);
+}
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+console.log("Loaeded firebase credentials:", serviceAccount.project_id || "No Project ID found");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports= admin;
--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB =require('./config/db');
+const admin= require('./firebase/firebaseConfig/firebaseConfig');
 const dotenv =require("dotenv");
 dotenv.config();
 const app= express();
@@ -8,6 +9,17 @@ const PORT=process.env.PORT || 5000;
 
 connectDB();
 
+app.get('/test', (req, res)=>{
+    admin.auth().listUsers(100)
+    .then((userRecords)=>{
+        console.log("Successfully fetched users",userRecords);
+        res.status(201).json({message:"Users fetched successfully",userRecords});
+    })
+    .catch((error)=>{
+    console.log("Error fethcing users",error);
+    res.status(500).json({message:"users not fetched"});
+    });
+})
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`);
 });
